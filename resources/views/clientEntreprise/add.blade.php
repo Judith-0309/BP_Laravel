6dd502e937cbb06d057159215bbb870da904f24c
Judith-0309 <sagnajudith@gmail.com>
2020-09-05 18:31:05 +0000

    modif

--- resources/views/clientEntreprise/add.blade.php
+++ resources/views/clientEntreprise/add.blade.php
@@ -4,7 +4,7 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
         <meta http-equiv="X-UA-Compatible" content="ie=edge"/>
-        <link rel="stylesheet" href="{$url_base}public/css/entreprise.css">
+        <link rel="stylesheet" href="{{ asset('css/entreprise.css')}}">
  
         <title>CLIENT Entreprise</title>
 
